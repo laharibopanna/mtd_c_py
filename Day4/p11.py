85a6427bb2b330d29c1d53c0c97795df68d11d05
laharibopanna <vvce22ec0111@vvce.ac.in>
2025-03-13 10:25:44 +0000

    p11

---
+++ Day4/p11.py
@@ -0,0 +1,5 @@
+input_number = int(input('Enter a number to print its Math table: '))
+
+for i in range(1, 21):
+    #print(f'{input_number} * {i} = {input_number * i}')
+    print('%d * %02d = %03d'%(input_number, i, input_number * i))
